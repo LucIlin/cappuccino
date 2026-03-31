6e9376095562c21bdbe82854362adab6609e4f75
Lucas Lindgren <lucas.lindgren@outlook.com>
2026-04-01 00:00:34 +0200

    feat: add logging to test configuration

--- src/Worker/Worker.cs
+++ src/Worker/Worker.cs
@@ -1,6 +1,9 @@
+using Microsoft.Extensions.Options;
+using Worker.Settings;
+
 namespace Worker;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker(ILogger<Worker> logger, IOptions<LLMConfiguration> _llmSettings) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -9,6 +12,12 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
             if (logger.IsEnabled(LogLevel.Information))
             {
                 logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                Console.WriteLine(
+                    $"LLM Settings: \n" +
+                    $"\n\tProvider: {_llmSettings.Value.Profiles["TestProfile"].Provider}" +
+                    $"\n\tApiKey: {_llmSettings.Value.Profiles["TestProfile"].ApiKey}" +
+                    $"\n\tModel: {_llmSettings.Value.Profiles["TestProfile"].Model}"
+                    );
             }
 
             await Task.Delay(1000, stoppingToken);
